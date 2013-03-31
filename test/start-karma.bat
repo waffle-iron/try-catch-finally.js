35ee906c8cd2daf94d83492a79ca1fa22ae47f4d
Chris Watson <chris24watson@gmail.com>
2013-03-31 13:06:27 +0100

    GH project page test

--- test/start-karma.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Karma try-catch-finally.js Runner
-set CHROME_BIN=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
-set OPERA_BIN=C:\Program Files (x86)\Opera\opera.exe
-
-set FIREFOX_BIN=C:\Program Files (x86)\Mozilla Firefox\firefox.exe
-set SAFARI_BIN=C:\Program Files (x86)\Safari\Safari.exe
-node ..\node_modules\karma\bin\karma start karma.conf.js
